0053035338401e3db9d7d9b8bdef054efb181555
Nazanin <earworm90@gmail.com>
2024-02-22 23:01:08 +0200

    Add components and all styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 // import logo from './logo.svg';
 import "./App.css";
+import Header from "./Components/Header";
+import Loading from "./Components/Loading";
+import Login from "./Components/Login";
 
 function App() {
+  console.log("i am inside the app");
   return (
-    <div className="App">
-      <h1 className="app-blue">Ready to build the SimpliMed plus</h1>
-    </div>
+    <>
+      <Header />
+      <Login />
+      {/* <Loading /> */}
+      <div className="App">
+        <h1 className="app-blue">RReady to build the SimpliMed plus</h1>
+      </div>
+    </>
   );
 }
 
