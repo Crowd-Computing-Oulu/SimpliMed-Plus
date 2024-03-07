8672da8fe8c0bcc3ca3efc878ecafa8641bb178b
Nazanin <earworm90@gmail.com>
2024-03-07 14:22:25 +0200

    add error

--- Client-Side/src/Components/GetAbstractBtn.js
+++ Client-Side/src/Components/GetAbstractBtn.js
@@ -1,13 +1,13 @@
 /*global chrome*/
 import React from "react";
-export default function GetAbstractBtn({ setState, updateState, abstract }) {
+export default function GetAbstractBtn({ setState, abstract }) {
   const style = {
     backgroundColor: "var(--secondary-color)",
     width: "80%",
     boxShadow: "0px 3px 5px 0px gray",
   };
+  const [error, setError] = React.useState("");
   async function getAbstract() {
-    // const abstractInformation = await getTabInformation(currentTab);
     console.log("the abstract in btn is", abstract);
     chrome.runtime.sendMessage(
       {
@@ -20,19 +20,28 @@ export default function GetAbstractBtn({ setState, updateState, abstract }) {
           ...prevState,
           ...response.state,
         }));
+        if (response.error) {
+          setError("There was an error, please try again");
+        }
       }
     );
   }
   return (
-    <div className="flex-grow-0 getAbstract-container d-flex align-items-center justify-content-center pt-3">
-      <button
-        id="getAbstract"
-        style={style}
-        className="  button "
-        onClick={getAbstract}
-      >
-        Get Abstract
-      </button>
+    <div className="getAbstract-container flex-grow-0">
+      <div className="  d-flex align-items-center justify-content-center pt-3">
+        <button
+          id="getAbstract"
+          style={style}
+          className="  button "
+          onClick={getAbstract}
+        >
+          Get Abstract
+        </button>
+      </div>
+      <p className="text-danger">{error}</p>
+      {/* <p className="mt-3 px-3">
+        Click the above button to get the simplified versions of this abstract
+      </p> */}
     </div>
   );
 }
