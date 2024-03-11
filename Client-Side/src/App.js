64d3d1ba4786d849218616efe6e58d9caa110d85
Nazanin <earworm90@gmail.com>
2024-03-11 16:18:14 +0200

    add routes and paths

--- Client-Side/src/App.js
+++ Client-Side/src/App.js
@@ -1,6 +1,13 @@
 /*global chrome*/
 
 import React from "react";
+import {
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+  createBrowserRouter,
+  createMemoryRouter,
+} from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header";
 import Loading from "./Components/Loading";
@@ -9,14 +16,20 @@ import Instructions from "./Components/Instructions";
 import Abstracts from "./Components/Abstracts";
 import AiAgent from "./Components/AiAgent";
 import GetSummary from "./Components/GetSummary";
+import Main from "./Components/Main";
+
+// import Navigation from "./Components/Navigation";
+import Layout from "./Components/Layout";
 import "bootstrap/dist/css/bootstrap-grid.min.css"; // Only import the grid system to avoid losing icons
 import { getTabInformation, updateState } from "./utils";
+import Error from "./pages/Error";
 // var port = chrome.runtime.connect({ name: "popupConnection" });
 
 function App() {
   const [username, setUsername] = React.useState(null);
   const [state, setState] = React.useState(null);
   const [abstract, setAbstract] = React.useState(null);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
   // Send a message each time the sidepanel opens
   React.useEffect(() => {
@@ -24,6 +37,10 @@ function App() {
       console.log("state upon open is", response.state);
       if (response.response === "TokenExist") {
         updateState(setState, response.state);
+        setIsLoggedIn(true);
+      } else {
+        console.log("Token does not exist");
+        setIsLoggedIn(false);
       }
     });
   }, []);
@@ -59,10 +76,6 @@ function App() {
         console.log("state will be update for the ");
         console.log(message.state);
         updateState(setState, message.state);
-        // setState((prevState) => ({
-        //   ...prevState,
-        //   ...message.state,
-        // }));
       }
     });
   }, []);
@@ -116,48 +129,58 @@ function App() {
       }
     };
     fetchTabData();
-    // console.log(abstract);
   }, []);
 
-  // what is the pint of this
-  // React.useEffect(() => {
-  //   chrome.storage.local.get(["accessToken", "username"], (data) => {
-  //     if (data.accessToken) {
-  //       console.log("storage is", data);
-  //       // This needs to be checked
-  //       updateState(setState, data);
-
-  //       // setState((prevState) => ({
-  //       //   ...prevState,
-  //       //   username: data.username,
-  //       //   accessToken: data.accessToken,
-  //       // }));
-  //       // setAccessToken(data.accessToken);
-  //     }
-  //   });
-  // }, []);
-  // end of the point
+  const router = createMemoryRouter(
+    createRoutesFromElements(
+      <Route
+        path="/"
+        errorElement={<Error />}
+        element={
+          state && <Layout state={state} handleLogout={handleLogoutChange} />
+        }
+      >
+        <Route
+          path="/login"
+          errorElement={<Error />}
+          element={
+            !isLoggedIn ? <Login handleLogin={handleLoginChange} /> : null
+          }
+        />
+        <Route path="/aiagent" errorElement={<Error />} element={<AiAgent />} />
+        <Route
+          path="/main"
+          errorElement={<Error />}
+          element={
+            <Main state={state} abstract={abstract} setState={setState} />
+          }
+        />
+      </Route>
+    )
+  );
 
   return (
     <div className="App">
-      {state && state.accessToken ? (
-        <>
-          <Header state={state} handleLogout={handleLogoutChange} />
-          {abstract && state && !state.isLoading && (
-            <GetSummary setState={setState} tabAbstract={abstract} />
-          )}
-          <AiAgent />
-        </>
-      ) : (
+      <RouterProvider router={router} />
+
+      {/* {
+        state && state.accessToken ? (
+          <>
+            <Header state={state} handleLogout={handleLogoutChange} />
+            {abstract && state && !state.isLoading && (
+              <GetSummary setState={setState} tabAbstract={abstract} />
+            )}
+          </>
+        ) : null
         <Login handleLogin={handleLoginChange} />
-      )}
+      }
 
       {state && !state.isLoading && !state.abstractData && <Instructions />}
       {state && !state.isLoading && state.abstractData && (
         <Abstracts abstracts={state.abstractData} />
       )}
 
-      {state && state.isLoading ? <Loading /> : ""}
+      {state && state.isLoading ? <Loading /> : ""} */}
     </div>
   );
 }
