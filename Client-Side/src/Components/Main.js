ab18428b41189adc677cf14b76affa3e45162000
Nazanin <earworm90@gmail.com>
2024-03-13 14:17:43 +0200

    Feat: add main component

---
+++ Client-Side/src/Components/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import GetSummary from "./GetSummary";
+import Abstracts from "./Abstracts";
+import Loading from "./Loading";
+import { AppContext } from "../App";
+export default function Main() {
+  const { state, abstract, setState } = React.useContext(AppContext);
+
+  return (
+    <>
+      <GetSummary setState={setState} tabAbstract={abstract} />
+      {state && !state.isLoading && state.abstractData && (
+        <Abstracts abstracts={state.abstractData} />
+      )}
+      {state && state.isLoading ? <Loading /> : null}
+    </>
+  );
+}
