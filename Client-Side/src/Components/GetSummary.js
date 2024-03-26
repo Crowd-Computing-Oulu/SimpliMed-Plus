662036a8dd9e04e2ff9f894ac1aca7da5b14816b
Nazanin <earworm90@gmail.com>
2024-03-26 17:23:18 +0200

    Feat: update get summary button

--- Client-Side/src/Components/GetSummary.js
+++ Client-Side/src/Components/GetSummary.js
@@ -4,8 +4,7 @@ import "./getSummary.css";
 import { AppContext } from "../App";
 import { updateState } from "../utils";
 export default function GetSummary() {
-  const { state, setState, abstract } = React.useContext(AppContext);
-
+  const { state, setState, abstract, wrongPage } = React.useContext(AppContext);
   const [error, setError] = React.useState("");
 
   async function getSummary() {
@@ -27,16 +26,55 @@ export default function GetSummary() {
     <div className="getSummary-container flex-grow-0">
       <div className="d-flex align-items-center justify-content-center pt-3">
         {!state.isLoading ? (
-          <button
-            className="button getSummaryBtn"
-            id="getSummary"
-            onClick={getSummary}
-          >
-            Get Summary
-          </button>
+          wrongPage ? (
+            <div className="d-flex flex-column align-items-center justify-content-center">
+              <p>
+                This page doesnt have any abstract content, Please go to pubmed
+                website and choose an article with an available abstract.
+              </p>
+              <button
+                className="button getSummaryBtn disabledBtn"
+                id="getSummary"
+              >
+                No available abstract
+              </button>
+            </div>
+          ) : state.abstractData ? (
+            state.abstractData.url === abstract.url ? (
+              <button
+                className="button getSummaryBtn disabledBtn"
+                id="getSummary"
+                // onClick={getSummary}
+              >
+                Summary Already Exists
+              </button>
+            ) : (
+              <div className="d-flex flex-column align-items-center justify-content-center">
+                <p className="text-warning">
+                  This is a new article, but the content you see is for previous
+                  article, get the new summary
+                </p>
+                <button
+                  className="button getSummaryBtn"
+                  id="getSummary"
+                  onClick={getSummary}
+                >
+                  Get Summary
+                </button>
+              </div>
+            )
+          ) : (
+            <button
+              className="button getSummaryBtn"
+              id="getSummary"
+              onClick={getSummary}
+            >
+              Get Summary
+            </button>
+          )
         ) : (
           <button
-            className="button getSummaryBtn disabledBtn "
+            className="button getSummaryBtn loadingBtn "
             id="getSummary"
             disabled
           >
