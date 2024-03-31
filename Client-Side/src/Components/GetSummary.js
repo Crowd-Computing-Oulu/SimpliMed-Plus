eb4c592abce2d224998bbd398bb49d25662f86a3
Nazanin <earworm90@gmail.com>
2024-03-31 19:23:38 +0300

    Feat: different buttons and status will be shown in get summary component

--- Client-Side/src/Components/GetSummary.js
+++ Client-Side/src/Components/GetSummary.js
@@ -3,8 +3,14 @@ import React from "react";
 import "./getSummary.css";
 import { AppContext } from "../App";
 import { updateState } from "../utils";
+import { Exclamation } from "react-bootstrap-icons";
 export default function GetSummary() {
   const { state, setState, abstract, wrongPage } = React.useContext(AppContext);
+  console.log("should be rerendered by swtiching tab");
+  console.log("abstract", abstract);
+  console.log("state", state);
+  console.log("wrongPage", wrongPage);
+
   const [error, setError] = React.useState("");
 
   async function getSummary() {
@@ -22,73 +28,88 @@ export default function GetSummary() {
       }
     );
   }
-  return (
-    <div className="getSummary-container flex-grow-0">
-      <div className="d-flex align-items-center justify-content-center pt-3">
-        {!state.isLoading ? (
-          wrongPage ? (
-            <div className="d-flex flex-column align-items-center justify-content-center">
-              <p>
-                This page doesnt have any abstract content, Please go to pubmed
-                website and choose an article with an available abstract.
+
+  function summaryButton() {
+    if (!state.isLoading) {
+      if (wrongPage) {
+        return (
+          <div className="d-flex flex-column align-items-center justify-content-center">
+            <p className="p-3">
+              This page doesn't have any abstract content. Please go to the
+              PubMed website and choose an article with an available abstract.
+            </p>
+            <button className="button getSummaryBtn disabledBtn">
+              No available abstract
+            </button>
+          </div>
+        );
+      } else {
+        if (error) {
+          return (
+            <>
+              <p style={{ color: "red" }} className="pt-3">
+                {error.message}
               </p>
-              <button
-                className="button getSummaryBtn disabledBtn"
-                id="getSummary"
-              >
-                No available abstract
+              <button className="button getSummaryBtn" onClick={getSummary}>
+                Get Summary
+              </button>
+            </>
+          );
+        } else if (
+          state.abstractData &&
+          state.abstractData.url === abstract.url
+        ) {
+          return (
+            <button className="button getSummaryBtn disabledBtn" disabled>
+              Summary Already Exists
+            </button>
+          );
+        } else if (
+          state.abstractData &&
+          state.abstractData.url !== abstract.url
+        ) {
+          return (
+            <div className="d-flex flex-column align-items-center justify-content-center">
+              <p className="text-warning p-3">
+                <Exclamation /> This is a new article, but the content you see
+                is for the previous article. Get the new summary
+              </p>
+              <button className="button getSummaryBtn" onClick={getSummary}>
+                Get Summary
               </button>
             </div>
-          ) : state.abstractData ? (
-            state.abstractData.url === abstract.url ? (
-              <button
-                className="button getSummaryBtn disabledBtn"
-                id="getSummary"
-                // onClick={getSummary}
-              >
-                Summary Already Exists
-              </button>
-            ) : (
-              <div className="d-flex flex-column align-items-center justify-content-center">
-                <p className="text-warning">
-                  This is a new article, but the content you see is for previous
-                  article, get the new summary
-                </p>
-                <button
-                  className="button getSummaryBtn"
-                  id="getSummary"
-                  onClick={getSummary}
-                >
-                  Get Summary
-                </button>
-              </div>
-            )
-          ) : (
-            <button
-              className="button getSummaryBtn"
-              id="getSummary"
-              onClick={getSummary}
-            >
+          );
+        } else if (!state.abstractData && abstract) {
+          return (
+            <button className="button getSummaryBtn" onClick={getSummary}>
               Get Summary
             </button>
-          )
-        ) : (
-          <button
-            className="button getSummaryBtn loadingBtn "
-            id="getSummary"
-            disabled
-          >
-            Summary Requested
-          </button>
-        )}
+          );
+        }
+      }
+    } else if (state.isLoading) {
+      return (
+        <button className="button getSummaryBtn loadingBtn" disabled>
+          Summary Requested
+        </button>
+      );
+    }
+  }
+
+  return (
+    <>
+      <div className="getSummary-container flex-grow-0">
+        <div className="d-flex align-items-center justify-content-center pt-3">
+          {state &&
+            summaryButton(
+              state.isLoading,
+              error,
+              state.abstractData,
+              abstract,
+              wrongPage
+            )}
+        </div>
       </div>
-      {state.isLoading
-        ? null
-        : error && (
-            <p style={{ color: "red" }} className="pt-3">
-              {error.message}
-            </p>
-          )}
-    </div>
+    </>
   );
 }
