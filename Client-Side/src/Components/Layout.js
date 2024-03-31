d03b8c018895d04ee666ac48120de96430679efb
Nazanin <earworm90@gmail.com>
2024-03-31 19:24:42 +0300

    Fix: header renders only when state exist

--- Client-Side/src/Components/Layout.js
+++ Client-Side/src/Components/Layout.js
@@ -3,14 +3,15 @@ import { Outlet, useLocation } from "react-router-dom";
 import Navigation from "./Navigation";
 import Header from "./Header";
 import Instructions from "./Instructions";
-
+import { AppContext } from "../App";
 export default function Layout() {
+  const { state } = React.useContext(AppContext);
   const location = useLocation();
   return (
     <>
-      <Header />
+      {state && <Header />}
       <Navigation />
-      {/* {location.pathname === "/" && <Instructions />} */}
+      {location.pathname === "/" && <Instructions />}
       <Outlet />
     </>
   );
