ee28f82428568ca34b1ea787512eddf32306623f
Nazanin <earworm90@gmail.com>
2024-04-15 15:58:48 +0300

    Fix: tooltip position on hover, when closed to the edge

--- Client-Side/src/Components/HighlightDefinition.js
+++ Client-Side/src/Components/HighlightDefinition.js
@@ -1,31 +1,18 @@
 import React, { useEffect } from "react";
 import { fetchWikipediaDefinition } from "../apis";
+import { WindowDash } from "react-bootstrap-icons";
+import "./highlightDefinition.css";
 export default function HighlightDefinition({ hardWords, text }) {
   const [modifiedOriginalText, setModifiedOriginalText] = React.useState(null);
-  const [tooltipVisible, setTooltipVisible] = React.useState({});
-  const [tooltipPosition, setTooltipPosition] = React.useState({ x: 0, y: 0 });
-
-  const styleDefinition = {
-    backgroundColor: "rgba(255, 216, 64, 1)",
-    position: "absolute",
-    minWidth: "200px",
-    color: "black",
-    padding: "0.5rem",
-    borderRadius: "0.5rem",
-    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-    zIndex: "1000",
-    wordWrap: "break-word",
-    textAlign: "left",
-    fontSize: "0.9rem",
-    fontFamily: "var(--primary-font)",
-    left: 20,
-    top: 20,
-  };
+  const tooltipRefs = React.useRef({}); // to hold tooltip position states
+  // const [tooltipStyle, setTooltipStyle] = React.useState({
+  //   display: "none",
+  // });
 
   //  This function highglight the hards words and show the definition on hover
   useEffect(() => {
     setModifiedOriginalText(showDefinition(text));
-  }, [tooltipVisible]);
+  }, [text]);
   function showDefinition(text) {
     let result = [];
     let remainingText = text;
@@ -49,8 +36,8 @@ export default function HighlightDefinition({ hardWords, text }) {
         result.push(
           <span
             key={result.length}
-            className="text-warningg"
-            style={{ color: "rgba(255, 165, 0, 1)", position: "relative" }}
+            className="highlitedWord"
+            style={{}}
             onMouseEnter={(event) => handleMouseEnter(lowerCaseWord, event)}
             onMouseLeave={() => handleMouseLeave(lowerCaseWord)}
           >
@@ -58,28 +45,19 @@ export default function HighlightDefinition({ hardWords, text }) {
               startIndex,
               startIndex + lowerCaseWord.length
             )}
-            <span
-              key={result.length}
-              className={tooltipVisible[word] ? "definitionOnHover" : "hidden"}
-              style={styleDefinition}
-            >
-              <span className="inline-block fw-bold">Wikipedia: </span>
-              <p>{definition}</p>
+            <span>
+              <p
+                key={result.length}
+                ref={(ref) => (tooltipRefs.current[lowerCaseWord] = ref)}
+                className="tooltip-definition hidden"
+                // style={tooltipStyle}
+              >
+                <span className="inline-block fw-bold">Wikipedia: </span>
+                {definition}
+              </p>
             </span>
           </span>
         );
-        // push the definition of the hard word to the result array
-        // result.push(
-        //   <span
-        //     key={result.length}
-        //     className={tooltipVisible[word] ? "definitionOnHover" : "hidden"}
-        //     style={styleDefinition}
-        //   >
-        //     <span className="inline-block fw-bold">Wikipedia: </span>
-        //     <p>{definition}</p>
-        //   </span>
-        // );
-
         remainingText = after;
       }
     });
@@ -91,25 +69,56 @@ export default function HighlightDefinition({ hardWords, text }) {
 
     return result;
   }
+  function calculateTooltipPosition(
+    clientX,
+    clientY,
+    tooltipWidth,
+    tooltipHeight,
+    word
+  ) {
+    // if (!tooltipRefs) return; // Ensure buttonRect is available
+    let tooltipLeft = 0 + 20;
+    let tooltipTop = 20;
+    if (clientX + tooltipWidth > window.innerWidth) {
+      let newTooltipX = window.innerWidth - tooltipWidth; // Adjusted left position
+      tooltipLeft = newTooltipX - clientX;
+    }
+    if (clientY + tooltipHeight > window.innerHeight) {
+      let newTooltipY = window.innerHeight - tooltipHeight; // Adjusted top position
+      tooltipTop = newTooltipY - clientY;
+    }
+    console.log("tooltip style should be set");
+    // setTooltipStyle({
+    //   display: "block",
+    //   backgroundColor: "gray",
+    //   left: `${tooltipLeft}px`,
+    //   top: `${tooltipTop}px`,
+    //   width: `${popupWidth}px`,
+    //   height: `${popupHeight}px`,
+    //   position: "absolute",
+    // });
+    const tooltip = tooltipRefs.current[word];
+    tooltip.className = "tooltip-definition";
+    tooltip.style.left = `${tooltipLeft}px`;
+    tooltip.style.top = `${tooltipTop}px`;
+  }
+
   // Event handler for entering a highlighted word
   const handleMouseEnter = (word, event) => {
-    console.log("mouse enter", word);
-    const { clientX, clientY } = event;
-    setTooltipPosition({ x: clientX, y: clientY });
-    setTooltipVisible((prevState) => ({
-      ...prevState,
-      [word]: true, // Set visibility state for the hovered word
-    }));
+    const tooltip = tooltipRefs.current[word];
+    if (tooltip) {
+      calculateTooltipPosition(event.clientX, event.clientY, 300, 100, word);
+    }
   };
   // Event handler for leaving a highlighted word
   const handleMouseLeave = (word) => {
-    console.log("mouse left", word);
-
-    setTooltipVisible((prevState) => ({
-      ...prevState,
-      [word]: false, // Set visibility state for the hovered word
-    }));
-    setTooltipPosition({ x: 0, y: 0 }); // Reset tooltip position
+    const tooltip = tooltipRefs.current[word];
+    if (tooltip) {
+      //reseting the position of the tooltip
+      tooltip.style.left = `0px`;
+      tooltip.style.top = `0px`;
+      tooltip.className = "hidden";
+    }
   };
 
   return (
