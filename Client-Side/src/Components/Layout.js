e25c5f60cc4d62a8130be6bb1a997a8832fa3a03
Nazanin <earworm90@gmail.com>
2024-04-29 13:16:52 +0300

    Feat: complete home page's styles and texts

--- Client-Side/src/Components/Layout.js
+++ Client-Side/src/Components/Layout.js
@@ -4,6 +4,8 @@ import Navigation from "./Navigation";
 import Header from "./Header";
 import Instructions from "./Instructions";
 import { AppContext } from "../App";
+import "./layout.css";
+import homeBG from "../../public/images/Home-Background2.png";
 export default function Layout() {
   const { state } = React.useContext(AppContext);
   const location = useLocation();
@@ -20,8 +22,32 @@ export default function Layout() {
     } else {
       return (
         <>
-          <h1> Welcome to SimpliMed, you need to login, click here</h1>
-          <NavLink to="/login">Login</NavLink>
+          <div className="home-section p-3 ">
+            <h1>Welcome to SimpliMed!</h1>
+            <div className="home-description mt-3">
+              <p className="">
+                It seems like you are not logged in, Please provide your OpenAi
+                Token!
+                <span> The token will be saved on your own computer!</span>
+              </p>
+            </div>
+            <img
+              alt="heart and books"
+              // src="../../public/images/Home-Background.png"
+              src={homeBG}
+            />
+            <NavLink to="/login">
+              <button
+                className="btn fw-bold mt-3 "
+                style={{
+                  backgroundColor: "var(--primary-color)",
+                  color: "var(--tertiary-color)",
+                }}
+              >
+                Go To Login Page
+              </button>
+            </NavLink>
+          </div>
         </>
       );
     }
