1ca3583ca5e7fca56aa329c2f6694aebe63215d3
Nazanin <earworm90@gmail.com>
2024-05-02 14:00:08 +0300

    Change: style and details for instructions

--- Client-Side/src/Components/Instructions.js
+++ Client-Side/src/Components/Instructions.js
@@ -1,14 +1,41 @@
 import React from "react";
-import "./instructions.css";
+import { NavLink } from "react-router-dom";
 
 export default function Instructions() {
   return (
-    <div
-      id="instructions-container"
-      className="instructions-container  hiddenn"
-    >
-      <h6 className="fw-bold">Instructions:</h6>
-      <ol>
+    <div className="information-container mt-5">
+      <h6 className="fw-bold px-3">Infromation:</h6>
+      <p className="px-4" style={{ textAlign: "justify" }}>
+        SimpliMed is a web-based application that helps users to find abstracts
+        based on medical questions. SimpliMed works on PubMed website and
+        simplifies articles abstracts. The 3 main sections consist of a chatbot
+        that connects users with an open ai agent, a summary sections that shows
+        different simplified versions to the users, and also help users to see
+        the difficult words meaning from wikipedia on hover. and the last
+        section helps users to see if they have found what they are looking for.
+        In order for simpliMed to work with openAI APIs, users need to enter
+        their OpenAI tokens. The consumptions will approximately be ... tokens
+        per abstract.
+      </p>
+
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <span style={{ fontFamily: "var(--secondary-font)" }}>
+          Start here by asking your medical quesiton:
+        </span>
+        <NavLink to="/chat">
+          <button
+            className="btn fw-bold mt-2"
+            style={{
+              backgroundColor: "var(--quaternary-color)",
+              color: "white",
+            }}
+          >
+            Go To Chat!
+          </button>
+        </NavLink>
+      </div>
+      {/* BELOW IS FOR THE USER STUDY */}
+      {/* <ol>
         <li>
           Please check your <b> Prolific username</b> from the top right corner
           of the page. If you have entered a wrong username, logout and login
@@ -28,7 +55,7 @@ export default function Instructions() {
           If you are going away from your keyword for few minutes, close the
           popup window by clicking outside of the popup!
         </li>
-      </ol>
+      </ol> */}
     </div>
   );
 }
