f9861e91e5809104ab0b8d53d68c56b8c1d80732
Nazanin <earworm90@gmail.com>
2024-05-21 15:39:45 +0300

    Feat: add names to the nav links

--- Client-Side/src/Components/Navigation.js
+++ Client-Side/src/Components/Navigation.js
@@ -2,51 +2,63 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { JournalMedical, ChatDots, InfoCircle } from "react-bootstrap-icons";
 export default function Navigation() {
-  const navLinkStyle = {
-    textDecoration: "none",
-    fontSize: "1rem",
-    fontWeight: "bold",
-    fontFamily: "var(--primary-font)",
-    color: "var(--primary-color)",
-  };
-  const activeStyle = {
-    textDecoration: "underline",
-    fontSize: "1rem",
-    color: "var(--quaternary-color)",
-    fontFamily: "var(--primary-font)",
-    fontWeight: "bolder",
-  };
-  const navStyle = {
-    padding: "0.5rem",
-    // borderBottom: "3px solid #c9c9c9",
-    height: "4rem",
-    boxShadow: "rgb(168, 80, 119) 10px 1px 3px",
+  const styles = {
+    nav: {
+      padding: "0.5rem",
+
+      // borderBottom: "3px solid #c9c9c9",
+      // height: "4rem",
+      boxShadow: "rgb(168, 80, 119) 1px 2px 1px",
+    },
+    navLink: {
+      textDecoration: "none",
+      fontSize: "1rem",
+      fontWeight: "bold",
+      fontFamily: "var(--primary-font)",
+      color: "var(--primary-color)",
+    },
+    active: {
+      textDecoration: "none",
+      fontSize: "1rem",
+      color: "var(--quaternary-color)",
+      fontFamily: "var(--primary-font)",
+      fontWeight: "bolder",
+    },
   };
 
   return (
     <>
       <nav
-        style={navStyle}
-        className="d-flex flex-row align-items-center justify-content-around"
+        style={styles.nav}
+        className="d-flex flex-row align-items-center justify-content-around mt-2"
         width="100%"
       >
         <NavLink
           to="/chat"
-          style={({ isActive }) => (isActive ? activeStyle : navLinkStyle)}
+          style={({ isActive }) => (isActive ? styles.active : styles.navLink)}
         >
-          <ChatDots size="1.8rem" />
+          <div className="d-flex align-items-center flex-column">
+            <ChatDots size="1.8rem" />
+            <div className="">Chat</div>
+          </div>
         </NavLink>
         <NavLink
           to="/abstracts"
-          style={({ isActive }) => (isActive ? activeStyle : navLinkStyle)}
+          style={({ isActive }) => (isActive ? styles.active : styles.navLink)}
         >
-          <JournalMedical size="1.8rem" />
+          <div className="d-flex align-items-center flex-column">
+            <JournalMedical size="1.8rem" />
+            <div>Abstracts</div>
+          </div>
         </NavLink>
         <NavLink
           to="help"
-          style={({ isActive }) => (isActive ? activeStyle : navLinkStyle)}
+          style={({ isActive }) => (isActive ? styles.active : styles.navLink)}
         >
-          <InfoCircle size="1.8rem" />
+          <div className="d-flex align-items-center flex-column">
+            <InfoCircle size="1.8rem" />
+            <div className="">Help</div>
+          </div>
         </NavLink>
       </nav>
     </>
