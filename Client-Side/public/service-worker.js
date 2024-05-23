078ccae5a40e6a1293a4477bd6de8fec9c4cebce
Nazanin <earworm90@gmail.com>
2024-05-23 11:43:46 +0300

    Feat: open side panel by clicking on action bar

--- Client-Side/public/service-worker.js
+++ Client-Side/public/service-worker.js
@@ -1,5 +1,10 @@
 /*global chrome*/
 
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
+
 // -------------------------
 // A state should have these values overall
 let state = {
