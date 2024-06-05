369b206192ec61f94e504745f7e557c47034fe53
Nazanin <earworm90@gmail.com>
2024-06-05 18:52:09 +0300

    Clean: remove extra code from header

--- Client-Side/src/Components/Header.js
+++ Client-Side/src/Components/Header.js
@@ -9,11 +9,6 @@ export default function Header() {
     <div id="header" className="header-container">
       <div className="d-flex flex-row justify-content-between">
         <span className="header-logo">SimpliMed</span>
-        {/* FOR THE USER STUDY - TO SHOW THE USER NAME  */}
-        {/* <span className="tooltip_1 header-username">
-          {state.username}
-          <span className="usernameTooltipText">Prolific Username</span>
-        </span> */}
         <NavLink to="/Login" onClick={handleLogoutChange}>
           <span className="logoutIcon-container tooltip_1" id="logoutBtn">
             <BoxArrowRight size="1.8rem" color="rgb(255, 69, 69)" />
@@ -21,23 +16,6 @@ export default function Header() {
           </span>
         </NavLink>
       </div>
-
-      {/* BELOW IS FOR THE USER STUDY - TO SHOW THE REMAINING DAILY TASKS FOR FEEDBACKS  */}
-      {/* {state.remainingFeedbacks === 0 && (
-        <span className="d-block" id="remainingFeedbacks">
-          No Remaining Feedbacks
-        </span>
-      )}
-      {state.remainingFeedbacks === 1 && (
-        <span className="d-block" id="remainingFeedbacks">
-          {state.remainingFeedbacks} Remaining Feedback{" "}
-        </span>
-      )}
-      {state.remainingFeedbacks > 1 && (
-        <span className="d-block" id="remainingFeedbacks">
-          {state.remainingFeedbacks} Remaining Feedbacks{" "}
-        </span>
-      )} */}
     </div>
   );
 }
