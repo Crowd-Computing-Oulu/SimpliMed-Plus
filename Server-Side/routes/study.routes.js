0f0bbdae53002b6de754a2e5e524275d0db20cd3
Nazanin <earworm90@gmail.com>
2024-06-05 19:12:34 +0300

    Remove Backend

--- Server-Side/routes/study.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const verifyToken = require("../middlewares/authToken");
-const { requestStudyStatus } = require("../controllers/study.controller.js");
-
-router.post("/status", verifyToken, requestStudyStatus);
-
-module.exports = router;
