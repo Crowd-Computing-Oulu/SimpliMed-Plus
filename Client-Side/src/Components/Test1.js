10f7616ce0ee58b237a0a2ff3783206d41f7124d
Nazanin <earworm90@gmail.com>
2024-06-07 14:17:43 +0300

    Clean: remove test files

--- Client-Side/src/Components/Test1.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function Test1() {
-  //   const error = useRouteError();
-  return (
-    <>
-      <h1 className="flex-grow-1">Hello Test1</h1>
-    </>
-  );
-}
--- Client-Side/src/Components/Test3.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function Test3() {
-  //   const error = useRouteError();
-  return (
-    <>
-      <h1 className="flex-grow-1">Hello Test3</h1>
-    </>
-  );
-}
